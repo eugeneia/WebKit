tomasz-karczewski-red/WPEWebKit

Add a property to WebKitWebsiteDataManager to set the LocalStorage quota.

Source/WebKit/UIProcess/WebsiteData/WebsiteDataStore.h
@@ -396,6 +396,7 @@ public:
     void setEmulatedConditions(std::optional<int64_t>&& bytesPerSecondLimit);
 #endif
 
+    unsigned localStorageQuota() const { return m_resolvedConfiguration->localStorageQuota(); }
 private:
     enum class ForceReinitialization : bool { No, Yes };
 #if ENABLE(APP_BOUND_DOMAINS)

eugeneia/WebKit

Source/WebKit/UIProcess/WebsiteData/WebsiteDataStore.h
@@ -439,6 +439,7 @@ public:
     void setEmulatedConditions(std::optional<int64_t>&& bytesPerSecondLimit);
 #endif
 
+    unsigned localStorageQuota() const { return m_configuration->localStorageQuota(); }
     void addPage(WebPageProxy&);
     void removePage(WebPageProxy&);
 